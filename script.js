2bbdc7c25ace2e2c3386de761a091bdbdae6232d
Anastasiia <150678939+AnastasiiaS25@users.noreply.github.com>
2024-02-02 13:28:51 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+menu.onclick = function myFunction(){
+    var x = document.getElementById('myTopnav');
+    if(x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+function calculate(){
+    var bmi;
+    var result = document.getElementById("result");
+var weight = parseInt(document.getElementById("weight").value);
+    document.getElementById("weight-val").textContent = weight + " kg";
+
+var height = parseInt(document.getElementById("height").value);
+    document.getElementById("height-val").textContent = height + " cm";
+var result = document.getElementById("result");
+var bmi;
+    bmi = (weight / Math.pow( (height/100), 2 )).toFixed(1);
+    result.textContent = bmi;
+    if(bmi < 18.5){
+        category = "Underweight 😒";
+        result.style.color = "#ffc44d";
+    }
+    else if( bmi >= 18.5 && bmi <= 24.9 ){
+        category = "Normal Weight 😍";
+        result.style.color = "#0be881";
+    }
+    else if( bmi >= 25 && bmi <= 29.9 ){
+        category = "Overweight 😮";
+        result.style.color = "#ff884d";
+    }
+    else{
+        category = "Obese 😱";
+        result.style.color = "#ff5e57";
+    }
+    document.getElementById("category").textContent = category;
+}
